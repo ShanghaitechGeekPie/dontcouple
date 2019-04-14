07d1f9bfaafe92d84ddad2b8ec7dca0bbef1efde
llk89 <27812632+llk89@users.noreply.github.com>
2019-04-14 18:56:07 +0800

    Update install_verilator.sh

--- travis/install_verilator.sh
+++ travis/install_verilator.sh
@@ -10,35 +10,45 @@ else
 	VERILATOR_VERSION=$1
 fi
 
+echo Expected verilator $VERILATOR_VERSION
+
 if [ -d "./verilator/bin" ] && [ "VERILATOR_VERSION" == "$(cat ./verilator/version)" ]
 then
-	# Valid cache, move stuff to where it belongs
-	sudo cp ./verilator/bin/* /usr/local/bin/ && sudo mkdir -p /usr/local/share/pkgconfig/ && sudo cp ./verilator/pkgconfig/* /usr/local/share/pkgconfig/ && sudo cp -r ./verilator/verilator/* /usr/local/share/verilator/
+	echo Valid cache found, moving stuff to where it belongs
+	sudo mkdir -vp /usr/local/share/pkgconfig/ /usr/local/share/verilator/ 
+	sudo cp ./verilator/bin/* /usr/local/bin/ && sudo cp ./verilator/pkgconfig/* /usr/local/share/pkgconfig/ && sudo cp -r ./verilator/verilator/* /usr/local/share/verilator/
 	exit $?
 fi
 
-# No cache or wrong version, build from scratch
+echo No cache or wrong version, rebuilding from scratch
 
 # Clean the cache first, in case version changed
 rm verilator -rf
 
 # Fetch
 wget "https://www.veripool.org/ftp/verilator-$VERILATOR_VERSION.tgz"
+echo Unpacking source tarball...
 tar -zxf verilator-*.tgz
 cd verilator-*
-
+echo Installing...
 # Install
 if ./configure && make && sudo make install
 then
 	# Prepare the cache for save
-	mkdir -p ../verilator/pkgconfig/
-	mkdir -p ../verilator/verilator/
+	echo Built. Creating cache...
+	mkdir -vp ../verilator/pkgconfig/ ../verilator/verilator/
+	echo Moving bin...
 	mv bin ../verilator/bin
+	echo Moving pkgconfig...
 	cp /usr/local/share/pkgconfig/* ../verilator/pkgconfig/
+	echo Moving include...
 	cp -r /usr/local/share/verilator/* ../verilator/verilator/
+	echo Writing version number to cache...
 	echo VERILATOR_VERSION > ../verilator/version
 	exit 0
 else
 	# Something went wrong
+	ERR_CODE=$?
+	echo Build returned with error code $ERR_CODE. Aborting...
 	exit $?
 fi
