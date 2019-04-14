3a15566cf0a48de3b10b88e130ff137665df3301
llk89 <27812632+llk89@users.noreply.github.com>
2019-04-14 16:11:11 +0800

    Update install_verilator.sh

--- travis/install_verilator.sh
+++ travis/install_verilator.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Copyright (c) 2019, llk89. Licensed to public under BSD 3-Clause.
 # Expecting caching /usr/local/share/verilator and /verilator.
-if [ -d "/verilator/bin" ]
+if [ -d "./verilator/bin" ]
 then
 	# has cache, move stuff to where it belongs
-	sudo mv /verilator/bin/* /usr/lib/bin && sudo mv /verilator/pkgconfig/* /usr/local/share/pkgconfig/ && sudo mv /verilator/verilator /usr/local/share/verilator
+	sudo mv ./verilator/bin/* /usr/lib/bin && sudo mv ./verilator/pkgconfig/* /usr/local/share/pkgconfig/ && sudo mv ./verilator/verilator /usr/local/share/verilator
 	exit $?
 fi
 
@@ -18,9 +18,9 @@ if ./configure && make && sudo make install
 then
 	# prepare the cache for save
 	mkdir -p /verilator/pkgconfig/
-	mv bin /verilator
-	cp /usr/local/share/pkgconfig/verilator* /verilator/pkgconfig/
-	cp /usr/local/share/verilator /verilator/verilator/
+	mv bin ../verilator
+	cp /usr/local/share/pkgconfig/verilator* ../verilator/pkgconfig/
+	cp /usr/local/share/verilator ../verilator/verilator/
 	exit 0
 else
 	# something went wrong
