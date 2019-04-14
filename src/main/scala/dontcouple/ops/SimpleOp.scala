6e26ef26caf749455b7ab9f135e7ed3ec9d2ba1b
luojw <luojw@shanghaitech.edu.cn>
2019-04-15 00:48:09 +0800

    Fixed typo.

--- src/main/scala/dontcouple/ops/SimpleOp.scala
+++ src/main/scala/dontcouple/ops/SimpleOp.scala
@@ -8,6 +8,7 @@ trait SimpleOp {
   val on_kick: () => Unit
   val on_done: () => Unit
   def kick(): Unit
+  def apply(): Unit
   def copy_but_on_kick(new_on_kick: () => Unit): SimpleOp
   def copy_but_on_done(new_on_done: () => Unit): SimpleOp
 }
