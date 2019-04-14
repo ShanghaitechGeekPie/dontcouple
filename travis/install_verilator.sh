82fbef8883e4620eea7a10d35ca0b54e6c3acc50
llk89 <27812632+llk89@users.noreply.github.com>
2019-04-14 16:45:32 +0800

    Implement version switch for verilator install

--- travis/install_verilator.sh
+++ travis/install_verilator.sh
@@ -1,28 +1,44 @@
 #!/bin/bash
 # Copyright (c) 2019, llk89. Licensed to public under BSD 3-Clause.
-# Expecting caching /usr/local/share/verilator and /verilator.
-if [ -d "./verilator/bin" ]
+# Expecting caching /verilator.
+# Accept a single argument to specify the verilator version.
+if [ -z "$1" ]
 then
-	# has cache, move stuff to where it belongs
+	# As suggested by chisel3, we use 4.006 here
+	VERILATOR_VERSION=4.006
+else
+	VERILATOR_VERSION=$1
+fi
+
+if [ -d "./verilator/bin" ] && [ "VERILATOR_VERSION" == "$(cat ./verilator/version)" ]
+then
+	# Valid cache, move stuff to where it belongs
 	sudo mv ./verilator/bin/* /usr/local/bin/ && sudo mv ./verilator/pkgconfig/* /usr/local/share/pkgconfig/ && sudo mv ./verilator/verilator /usr/local/share/verilator
 	exit $?
 fi
 
-# No cache, build from scratch
-# As suggested by chisel3, we use 4.006 here
-wget https://www.veripool.org/ftp/verilator-4.006.tgz
-tar -zxvf verilator-4.006.tgz
-cd verilator-4.006
-# install
+# No cache or wrong version, build from scratch
+
+# Clean the cache first, in case version changed
+rm verilator -rf
+
+# Fetch
+wget "https://www.veripool.org/ftp/verilator-$(VERILATOR_VERSION).tgz"
+tar -zxvf verilator-*.tgz
+cd verilator-*
+
+# Install
 if ./configure && make && sudo make install
 then
-	# prepare the cache for save
-	mkdir -p /verilator/pkgconfig/
+	# Prepare the cache for save
+	mkdir -p ../verilator/pkgconfig/
+	mkdir -p ../verilator/verilator/
 	mv bin ../verilator
-	cp /usr/local/share/pkgconfig/verilator* ../verilator/pkgconfig/
-	cp /usr/local/share/verilator ../verilator/verilator/
+	cp /usr/local/share/pkgconfig/* ../verilator/pkgconfig/
+	cp /usr/local/share/verilator/* ../verilator/verilator/*
+	echo VERILATOR_VERSION > ../verilator/version
 	exit 0
 else
-	# something went wrong
+	# Something went wrong
 	exit $?
 fi
